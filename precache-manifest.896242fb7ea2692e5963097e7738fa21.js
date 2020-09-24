0dda8bfb593f1fe69c385ebf5800d6ddd6479f8d
sikang <updream4@naver.com>
2020-09-25 08:35:52 +0900

    Updates

---
+++ precache-manifest.896242fb7ea2692e5963097e7738fa21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8becffe9c0fb4b4935ecb8fc39dac2",
+    "url": "/todolistsk/index.html"
+  },
+  {
+    "revision": "ccc6c2518d94f08a374d",
+    "url": "/todolistsk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1b11dc534b1bee734e62",
+    "url": "/todolistsk/static/js/2.6e5befd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolistsk/static/js/2.6e5befd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc6c2518d94f08a374d",
+    "url": "/todolistsk/static/js/main.357b3433.chunk.js"
+  },
+  {
+    "revision": "c8ea93b3761d688e18cc",
+    "url": "/todolistsk/static/js/runtime-main.919ae3c1.js"
+  }
+]);
